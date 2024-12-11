74f9dcdafba0dee94ebad56905e43b68798d3145
Jonathan Edwards <88948334+euplayer2@users.noreply.github.com>
2024-12-11 18:13:58 -0300

    feat: Coloca o Lombok no projeto

--- src/main/java/dev/jon/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/jon/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.jon.CadastroDeNinjas.Missoes;
 
 import dev.jon.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/jon/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/jon/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,15 @@ package dev.jon.CadastroDeNinjas.Ninjas;
 
 import dev.jon.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_-cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -19,37 +25,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
